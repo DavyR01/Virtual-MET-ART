434099bb72d60a49c6122b2da62f8905e7a8f8bc
Davy <100140271+DavyR01@users.noreply.github.com>
2024-11-13 11:18:43 +0100

    move @vitejs/plugin-react from devDependencies into dependencies & rename config file into vite.config.mjs to recognize ESJ instead of CJS (module treatment)

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,8 +1,7 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
+import path from "path";
 // const path = require("path");
-import path from 'path';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
