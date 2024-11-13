15ec67a7fddb16eb85691d484ca691a78e37ec2d
Davy <100140271+DavyR01@users.noreply.github.com>
2024-11-13 10:52:43 +0100

    Change type module and the way to import module (commonJs into module) in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,8 @@
-const path = require("path");
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// const path = require("path");
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
