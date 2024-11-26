849c3d8cd821ca7fa59a807a7184488f3d438149
Davy <100140271+DavyR01@users.noreply.github.com>
2024-11-26 21:24:11 +0100

    Add modifications vite.config.mjs file

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -4,22 +4,25 @@ import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, "src/components"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@services": path.resolve(__dirname, "src/services"),
-      "@media": path.resolve(__dirname, "media"),
-      "@public": path.resolve(__dirname, "public"),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "src/components"),
+        "@pages": path.resolve(__dirname, "src/pages"),
+        "@services": path.resolve(__dirname, "src/services"),
+        "@media": path.resolve(__dirname, "media"),
+        "@public": path.resolve(__dirname, "public"),
+      },
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    port: process.env.PORT || 3000,
-    //  fs: {
-    //    allow: ["./media"],
-    //  },
-  },
+    server: {
+      host: "0.0.0.0",
+      port: mode === "production" ? process.env.PORT || 3000 : 8090, // Exemple de port différent pour prod et dev
+    },
+    build: {
+      // Configuration spécifique à la production (minification, etc.)
+      minify: mode === "production" ? "esbuild" : false, // Minification en production uniquement
+    },
+  };
 });
